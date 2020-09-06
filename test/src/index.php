69fbaaa4d448acc8aa0ee3e13057e5c18c5a4fc0
pro-ghanem <holywater150@gmail.com>
2020-09-06 02:51:21 +0200

    Update index.php

--- test/src/index.php
+++ test/src/index.php
@@ -1,22 +1,15 @@
 <?php
-echo "Inside K8s with MySQL <br>";
-$conn = new mysqli("mysql", "db_user","ghanem");
+$servername = "mysql";
+$username = "db_user";
+$password = "ghanem";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT name FROM users";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-	// output data of each row
-	while($row = $result->fetch_assoc()) {
-		echo $row['name']."<br>";
-	}
-} else {
-	echo "0 results";
-}
 // Create database
 $sql = "CREATE DATABASE my_db";
 if ($conn->query($sql) === TRUE) {
@@ -24,27 +17,6 @@ if ($conn->query($sql) === TRUE) {
 } else {
   echo "Error creating database: " . $conn->error;
 }
-    
-
-
-// Check connection
-$conn = new mysqli("mysql", "db_user","ghanem","my_db");
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-
-  
-
-$sql = "INSERT INTO my_db (firstname, lastname, email)
-VALUES ('ahmed', 'ghanem', 'pro.ahmedghanem@gmail.com')";
-
-if ($conn->query($sql) === TRUE) {
-  $last_id = $conn->insert_id;
-  echo "New record created successfully. Last inserted ID is: " . $last_id;
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
 
 $conn->close();
+?>
